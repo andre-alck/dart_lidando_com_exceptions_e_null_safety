eeabcd86089af9693c7d9517946634051dd4c1a3
andre-alck <andrealck1@gmail.com>
2022-11-23 13:19:11 -0300

    first commit

---
+++ bin/dart_lidando_com_exceptions_e_null_safety.dart
@@ -0,0 +1,5 @@
+import 'package:dart_lidando_com_exceptions_e_null_safety/dart_lidando_com_exceptions_e_null_safety.dart' as dart_lidando_com_exceptions_e_null_safety;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_lidando_com_exceptions_e_null_safety.calculate()}!');
+}
