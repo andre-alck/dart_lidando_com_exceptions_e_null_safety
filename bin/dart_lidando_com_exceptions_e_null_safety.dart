3a032906b225485d9b1b48950a57829c8094a6bc
andre-alck <andrealck1@gmail.com>
2022-11-25 17:31:43 -0300

    feat: add 1/2 1

--- bin/dart_lidando_com_exceptions_e_null_safety.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_lidando_com_exceptions_e_null_safety/dart_lidando_com_exceptions_e_null_safety.dart' as dart_lidando_com_exceptions_e_null_safety;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_lidando_com_exceptions_e_null_safety.calculate()}!');
-}
--- test/dart_lidando_com_exceptions_e_null_safety_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_lidando_com_exceptions_e_null_safety/dart_lidando_com_exceptions_e_null_safety.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
